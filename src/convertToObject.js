1c35a4756700f02538636b7f471f54e0c1593767
Bauzi <volodiasyd@gmail.com>
2025-09-18 15:48:06 +0300

    Solution #3

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,11 @@ function convertToObject(sourceString) {
     .filter(Boolean)
     .reduce((prev, line) => {
       const [prop, ...value] = line.split(':');
+      const key = prop.trim();
 
+      if (!key || value.length === 0) {
+        return prev;
+      }
       prev[prop.trim()] = value.join(':').trim();
 
       return prev;
